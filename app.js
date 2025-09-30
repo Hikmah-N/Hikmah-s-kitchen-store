c65e7d078daf53a973ee4d9d95dc72be61fbbd20
Hikmah-N <hikmahnurudeen20@gmail.com>
2025-09-30 12:33:29 +0100

    update app

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
     const getUserDetails = JSON.parse (localStorage.getItem("users")) || [];
     const foundUser = getUserDetails.find((user)=>user.email === email);
 
-        const errorText = document.querySelector(".errorText");
+    let errorText = document.querySelector(".errorText");
 
     if(foundUser.password === password && foundUser.email === email) {
-        errorText.textContent = "You are logged in"
+        errorText.textContent = "You are logged in";
+        window.location.replace("./pages/Home.html");
     } else{
         errorText.textContent = "Invalid email or password"
+        return;
     }
-
-    window.location.replace("./pages/Home.html")
 }); 
 
